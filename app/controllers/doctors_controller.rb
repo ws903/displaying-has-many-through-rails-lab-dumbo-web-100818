f4469317220b6adaf5675513c4c2c340a553b202
ws903 <ws903@nyu.edu>
2018-11-07 16:37:12 -0500

    Done.

---
+++ app/controllers/doctors_controller.rb
@@ -0,0 +1,23 @@
+class DoctorsController < ApplicationController
+  def index
+  end
+
+  def show
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
